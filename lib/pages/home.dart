1e7764da5c43b6249f30fcf01f2581734c670bdd
ThatsEli <public@thatseliyt.de>
2019-07-28 14:18:15 +0200

    added submission loading; added basic post preview

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,6 @@
+import 'package:draw/draw.dart';
 import 'package:flutter/material.dart';
+import 'package:redditclient/stores/redditStore.dart';
 import 'package:redditclient/widgets/post.dart';
 
 class Home extends StatefulWidget {
@@ -8,14 +10,39 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  List<Submission> submissions;
+
+  @override
+  void initState() {
+    this.getPosts();
+    super.initState();
+  }
+
+  Future<void> getPosts() async {
+    List<Submission> userContents = <Submission>[];
+    await RedditStore.reddit.front.hot(params: { 'limit': '100' }).forEach((UserContent userContent) {
+      userContents.add(userContent as Submission);
+    });
+    setState(() {
+      this.submissions = userContents;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Column(
-         children: <Widget>[
-           Post(),
-         ],
-       ),
+    return Material(
+       child: this.submissions != null ?
+        ListView.builder(
+          itemCount: this.submissions.length,
+          itemBuilder: (BuildContext context, int index) {
+            if(index % 2 == 1) {
+              return Post(submission: this.submissions[index~/2 + 1]);
+            } else {
+              return Divider();
+            }
+          }
+        ) : Text('Loading...'),
     );
   }
 }
--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -1,78 +1,68 @@
+import 'package:draw/draw.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class Post extends StatefulWidget {
-  Post({Key key}) : super(key: key);
+  final Submission submission;
+
+  Post({Key key, this.submission}) : super(key: key);
+  
 
   _PostState createState() => _PostState();
 }
 
 class _PostState extends State<Post> {
+
   @override
   Widget build(BuildContext context) {
     return Material(
       child: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Container(
-           child: Column(
-             children: <Widget>[
-               Column(
-                 children: <Widget>[
-                   Text(
-                     'This is a test title of a test post for Reddit that is long enough for testing',
-                     maxLines: 3,
-                     style: TextStyle(
-                       fontSize: 25
-                     )
-                   ),
-                   Row(
-                     children: <Widget>[
-                        Text(
-                          'r/testSubReddit',
-                          style: TextStyle(
-                            fontSize: 15
-                          ),
-                        ),
-                        Text(
-                          ' - ',
-                          style: TextStyle(
-                            fontSize: 15
-                          ),
-                        ),
-                        Text(
-                          'u/testUser',
-                          style: TextStyle(
-                            fontSize: 15
-                          ),
-                        )
-                     ],
-                   ),
+          child: Column(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Text(
+                      widget.submission.title,
+                      maxLines: 3,
+                      style: TextStyle(fontSize: 25)),
                   Row(
                     children: <Widget>[
                       Text(
-                        '1537 Votes',
-                        style: TextStyle(
-                          fontSize: 20
-                        ),
+                        'r/' + widget.submission.subreddit.displayName,
+                        style: TextStyle(fontSize: 15),
                       ),
                       Text(
                         ' - ',
-                        style: TextStyle(
-                          fontSize: 20
-                        ),
+                        style: TextStyle(fontSize: 15),
                       ),
                       Text(
-                        '475 Comments',
-                        style: TextStyle(
-                          fontSize: 20
-                        ),
+                        'u/' + widget.submission.author,
+                        style: TextStyle(fontSize: 15),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Text(
+                        widget.submission.upvotes.toString() + ' Votes',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Text(
+                        ' - ',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Text(
+                        widget.submission.numComments.toString() + ' Comments',
+                        style: TextStyle(fontSize: 20),
                       )
                     ],
                   )
-                 ],
-               )
-             ],
-           ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
