030b3532bfa7df4c58ae807fa74a6f43835e1140
ThatsEli <public@thatseliyt.de>
2019-07-28 14:54:52 +0200

    added image, votes % saving to posts;

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -6,15 +6,27 @@ class Post extends StatefulWidget {
   final Submission submission;
 
   Post({Key key, this.submission}) : super(key: key);
-  
 
   _PostState createState() => _PostState();
 }
 
 class _PostState extends State<Post> {
+  VoteState votedState;
+  bool saved;
+
+  @override
+  void initState() {
+    this.votedState = widget.submission.vote;
+    this.saved = widget.submission.saved;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
+    String imageUrl = (widget.submission.preview.length > 0)
+        ? widget.submission.preview.elementAt(0).source.url.toString()
+        : '';
+
     return Material(
       child: Padding(
         padding: const EdgeInsets.all(10.0),
@@ -23,25 +35,40 @@ class _PostState extends State<Post> {
             children: <Widget>[
               Column(
                 children: <Widget>[
-                  Text(
-                      widget.submission.title,
-                      maxLines: 3,
-                      style: TextStyle(fontSize: 25)),
-                  Row(
-                    children: <Widget>[
-                      Text(
-                        'r/' + widget.submission.subreddit.displayName,
-                        style: TextStyle(fontSize: 15),
-                      ),
-                      Text(
-                        ' - ',
-                        style: TextStyle(fontSize: 15),
-                      ),
-                      Text(
-                        'u/' + widget.submission.author,
-                        style: TextStyle(fontSize: 15),
-                      )
-                    ],
+                  imageUrl != ''
+                      ? ClipRRect(
+                          borderRadius: BorderRadius.circular(4),
+                          child: Image.network(
+                            imageUrl,
+                            height: MediaQuery.of(context).size.width * 0.56279,
+                            width: MediaQuery.of(context).size.width,
+                            alignment: Alignment.center,
+                            fit: BoxFit.cover,
+                          ),
+                        )
+                      : null,
+                  Text(widget.submission.title,
+                      maxLines: 3, style: TextStyle(fontSize: 25)),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Row(
+                      children: <Widget>[
+                        Text(
+                          'r/' + widget.submission.subreddit.displayName,
+                          style:
+                              TextStyle(fontSize: 15, color: Colors.redAccent),
+                        ),
+                        Text(
+                          ' - ',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        Text(
+                          'u/' + widget.submission.author,
+                          style:
+                              TextStyle(fontSize: 15, color: Colors.blueAccent),
+                        )
+                      ],
+                    ),
                   ),
                   Row(
                     children: <Widget>[
@@ -58,6 +85,74 @@ class _PostState extends State<Post> {
                         style: TextStyle(fontSize: 20),
                       )
                     ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: <Widget>[
+                        GestureDetector(
+                          child: Icon(
+                            Icons.arrow_upward,
+                            color: this.votedState == VoteState.upvoted
+                                ? Colors.red
+                                : null,
+                            size: 30,
+                          ),
+                          onTap: () {
+                            setState(() {
+                              VoteState newVoteState =
+                                  this.votedState == VoteState.upvoted
+                                      ? VoteState.none
+                                      : VoteState.upvoted;
+                              this.votedState = newVoteState;
+                              if (newVoteState == VoteState.upvoted)
+                                widget.submission.upvote();
+                              else
+                                widget.submission.clearVote();
+                            });
+                          },
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                          child: GestureDetector(
+                            child: Icon(Icons.arrow_downward,
+                                color: this.votedState == VoteState.downvoted
+                                    ? Colors.blue
+                                    : null,
+                                size: 30),
+                            onTap: () {
+                              setState(() {
+                                VoteState newVoteState =
+                                    this.votedState == VoteState.downvoted
+                                        ? VoteState.none
+                                        : VoteState.downvoted;
+                                this.votedState = newVoteState;
+                                if (newVoteState == VoteState.downvoted)
+                                  widget.submission.downvote();
+                                else
+                                  widget.submission.clearVote();
+                              });
+                            },
+                          ),
+                        ),
+                        GestureDetector(
+                          child: Icon(this.saved ? Icons.favorite : Icons.favorite_border,
+                              color: this.saved ? Colors.yellow : null,
+                              size: 30),
+                          onTap: () {
+                            setState(() {
+                              if(this.saved) {
+                                widget.submission.unsave();
+                                this.saved = false;
+                              } else {
+                                widget.submission.save();
+                                this.saved = true;
+                              }
+                            });
+                          },
+                        )
+                      ],
+                    ),
                   )
                 ],
               )
